e1cce2b3c2f81d00a140779916c7de5b8d33d4e5
yvt <i@yvt.jp>
2026-08-11 13:15:55 +0900

    feat(flake): add `packages.aevenhub-simulator-headless`

--- flake.nix
+++ flake.nix
@@ -15,6 +15,18 @@
         evenhub-simulator = pkgs.callPackage ./sim-linux-x64.nix { };
 
         default = evenhub-simulator;
+
+        evenhub-simulator-headless = pkgs.writeShellApplication {
+          name = "evenhub-simulator-headless";
+          runtimeInputs = [ evenhub-simulator pkgs.xvfb-run ];
+          text = ''
+            #! ${pkgs.runtimeShell}
+            export LIBGL_DRIVERS_PATH=${pkgs.mesa}
+            export __EGL_VENDOR_LIBRARY_FILENAMES=${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json
+            export WEBKIT_DISABLE_COMPOSITING_MODE=1
+            exec xvfb-run --auto-display evenhub-simulator "$@"
+          '';
+        };
       };
   };
 }
